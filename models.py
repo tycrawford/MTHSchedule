f01ff24a03b7d325a11351d2508d531e13208e87
tycrawford <tycrawford314@gmail.com>
2017-11-24 20:17:55 -0600

    Tied to MAMP database

---
+++ models.py
@@ -0,0 +1,23 @@
+from app import db
+import random
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(20), unique=True)
+#     pwHash = db.Column(db.String(120))
+#     #games = db.relationship('Games', backref='' #maybe connect with character, which has a game id column instead of games
+#     gamesPlayed = db.Column(db.Integer, default=0)
+#     score = db.Column(db.Integer, default=1000)
+
+#     def __init__(self, username, password):
+#         self.username = username
+#         self.pwHash = makePwHash(password)
+
+class Employee(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    employeeName = db.Column(db.String(50))
+
+class Request(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    employeeID = db.Column(db.Integer)
+    
